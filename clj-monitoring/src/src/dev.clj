aa23ee4df0518a1678fff4b1504ee9f0cf1a0232
willyb22 <willyb4project@gmail.com>
2024-10-14 11:58:55 +0700

    update dev.clj

--- clj-monitoring/src/src/dev.clj
+++ clj-monitoring/src/src/dev.clj
@@ -1,8 +1,6 @@
 (ns dev
   (:require [app.core :as core]
-            [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.java.shell :as shell]
-            [clojure.string :as cstr]))
+            [clojure.tools.namespace.repl :refer [refresh]]))
 
 (defonce server (atom nil))
 
@@ -16,13 +14,6 @@
     (.stop @server)
     (reset! server nil)
     (println "server stopped")))
-
-;; (defn kill-process []
-;;   (let [pid (-> (shell/sh "lsof" "-t" (str "-i:" 3000))
-;;                 :out
-;;                 cstr/trim)]
-;;     (when (not (empty? pid))
-;;       (shell/sh "kill" "-9" pid))))
 
 (defn restart-server []
   (stop-server)
